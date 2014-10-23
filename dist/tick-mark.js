48efd5720489c5cb34beb369505b655439bc41d9
chris.scott@factmint.com <chris.scott@factmint.com>
2014-10-23 16:17:44 +0000

    Removed number rendering from the tick mark as the axis plugin not does that work if a label is not provided

--- dist/tick-mark.js
+++ dist/tick-mark.js
@@ -1,5 +1,5 @@
-define(['snap', 'Config', 'number-utils'],
-function(Snap,   Config,   NumberUtils) {
+define(['snap', 'config'],
+function(Snap,   Config) {
 	return Snap.plugin(function(Snap, Element, Paper) {
 
 		/**
@@ -14,7 +14,7 @@ function(Snap,   Config,   NumberUtils) {
 			var paper = this;
 
 			if (orientation === 'horizontal') {
-				var text = this.text(x - Config.TICK_MARK_HORIZONTAL_PADDING, y, NumberUtils.renderValue(value))
+				var text = this.text(x - Config.TICK_MARK_HORIZONTAL_PADDING, y, value)
 					.attr({
 						'font-family': Config.FONT_FAMILY,
 						'font-size': Config.TEXT_SIZE_SMALL,
@@ -26,7 +26,7 @@ function(Snap,   Config,   NumberUtils) {
 					.addClass('fm-tick-mark');
 			} else if (orientation === 'vertical') {
 				var line = this.line(x, y, x, y + size);
-				var text = this.text(x, y + Config.TICK_MARK_VERTICAL_PADDING + Config.TEXT_SIZE_SMALL / 2, NumberUtils.renderValue(value))
+				var text = this.text(x, y + Config.TICK_MARK_VERTICAL_PADDING + Config.TEXT_SIZE_SMALL / 2, value)
 					.attr({
 						'font-family': Config.FONT_FAMILY,
 						'font-size': Config.TEXT_SIZE_SMALL,
